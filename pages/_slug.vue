a5325b3bfa1100f0cab7ace215c7383f1e5e085c
aureliasegarra <av.segarra@gmail.com>
2021-09-05 08:44:46 +0200

    :construction: fetching data

---
+++ pages/_slug.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <img :src="planet.image" alt="" class="planet">
+    <h1>{{ planet.title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    const planet = await fetch(
+      `https://api.nuxtjs.dev/planets/${params.slug}`
+    ).then((res) => {
+      if(res.ok) {
+        return res.json()
+      }
+      throw new Error(res.status)
+    })
+    return { planet }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.link {
+  margin: auto;
+  font-size: 30px;
+}
+
+.planet {
+  width: 10%;
+  margin-top: 40px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,49 @@
 <template>
   <div class="container">
-    <Tutorial/>
-    <NuxtLink to="/about">About</NuxtLink>
+    <NuxtLogo />
+    <NuxtLink to="/about" class="link">About</NuxtLink>
+    <p v-if="$fetchState.pending">Fetching planets ...</p>
+    <p v-else-if="$fetchState.error">Error while fetching planets ...</p>
+    <ul>
+      <li v-for="planet in planets" :key="planet.slug">
+        <NuxtLink :to="planet.slug">{{ planet.title }}</NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      planets: []
+    }
+  },
+  async fetch() {
+    this.planets = await fetch(
+      'https://api.nuxtjs.dev/planets'
+    ).then((res) => res.json())
+  }
+}
 </script>
 
 <style scoped>
 .container {
-  margin: auto;
+  margin: 0;
+  min-height: 100vh;
   display: flex;
-  flex-direction: column;
   justify-content: center;
-  align-items: center;
+  flex-direction: column;
+  text-align: center;
+}
+
+.link {
+  margin: auto;
+  font-size: 30px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
 }
 </style>
