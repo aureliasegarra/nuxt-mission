d6cac07422972755293f244ba0c8cf47ad164946
aureliasegarra <av.segarra@gmail.com>
2021-09-19 15:13:04 +0200

    :construction: update navBar

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-project',
+    title: 'nuxt-mission',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'A mission to explore the Jamstack using Nuxt.' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
